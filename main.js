ff2a5c5e4e6fd459acf6bbd39758bcc554187146
Brancen Gregory <brancengregory@gmail.com>
2018-04-18 10:37:30 -0500

    Adding responsiveness

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-/* var newbox = document.createElement("section");
-var newdiv = document.createElement("div");
-var newhead = document.createElement("h2");
-newbox.appendChild(newdiv);
-newdiv.appendChild(newhead);
-newdiv.className = "section-title";
-newhead.innerHTML = ("Test");
-var test = document.body.children[1];
-test.appendChild(newbox); */
+var skilllevels = document.getElementsByClassName('skill-container');
+var skillval = skilllevels.item(1).getAttribute('style').split(': ')[1];
+console.log(skillval);
+
